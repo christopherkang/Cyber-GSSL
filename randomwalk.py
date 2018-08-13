bd2fedd1c7372c94ee8c631ba37a60df2e72d1de
CK <kang828@we23533.pnl.gov>
2018-08-13 15:38:42 -0700

    Tried to make linting related changes

--- randomwalk.py
+++ randomwalk.py
@@ -4,7 +4,7 @@ import sys
 import random
 import numpy as np
 
-IN_FILENAME = "NN_NNMF_500Cves.txt" #set 
+IN_FILENAME = "NN_NNMF_500Cves.txt" #set
 IMPORT_ARRAY = np.loadtxt(os.path.join(sys.path[0],IN_FILENAME), delimiter = ' ')
 
 MAX_ITERS = 100
@@ -24,22 +24,22 @@ print("This graph has %d nodes and %d edges"%(NUMBER_OF_NODES,NUMBER_OF_EDGES))
 LABEL_LIST = np.zeros((NUMBER_OF_NODES+1,1))-1
 LABEL_LIST[4] = 1
 
-INDEX_MARKERS = [x for x in range(IMPORT_ARRAY.shape[0]) if IMPORT_ARRAY[x][0] != IMPORT_ARRAY[x-1][0]]
+INDEX_MARKERS = [x for x in range(IMPORT_ARRAY.shape[0])
+                if IMPORT_ARRAY[x][0] != IMPORT_ARRAY[x-1][0]]
 INDEX_MARKERS.append(IMPORT_ARRAY.shape[0])
 
 #this generates a list of lists - the first index is the node's index
 #and the list inside signifies all of the nodes' connections
-NODE_CONNECTIONS = [[int(IMPORT_ARRAY[x][1]) for x in 
-    range(INDEX_MARKERS[y],INDEX_MARKERS[y+1])]
-    for y in range(max(NODE_LIST))]
+NODE_CONNECTIONS = [[int(IMPORT_ARRAY[x][1]) for x in range(INDEX_MARKERS[y],INDEX_MARKERS[y+1])]
+                    for y in range(max(NODE_LIST))]
 NODE_CONNECTIONS.insert(0, 0)
 
 def shuffle_list(inp_list):
-    """Automatically shuffles a list, nondestructively
-    
+    """Automatically shuffles a given list, nondestructively
+   
     Arguments:
         inp_list {List} -- input list
-    
+   
     Returns:
         list -- shuffled list
     """
@@ -48,19 +48,19 @@ def shuffle_list(inp_list):
     return temp_list
 
 def check_neighbor(inp_node, connection_list, time_label_list):
-    """Checks the neighbors of a node and outputs the most common labels. 
+    """Checks the neighbors of a node and outputs the most common labels.
     If there is a tie, the label is randomly chosen.
     This is one of the propagative functions.
-    
+   
     Arguments:
         inp_node {int} -- integer describing the index of the node
         connection_list {nested lists} -- defined in node_connections
         time_label_list {list} -- list with each node index assigned to a label
-    
+   
     Returns:
         int -- recommended label for that node.
     """
-    temp_label_hold = []    
+    temp_label_hold = []
     for neighbors in connection_list[inp_node]:
         if time_label_list[neighbors] == -1: #only count "real" labels
             pass
@@ -70,27 +70,26 @@ def check_neighbor(inp_node, connection_list, time_label_list):
     print(temp_label_hold)
     c=temp_label_hold.count #shorting
     top_labels = list({x for x in temp_label_hold if c(x)==max(map(c,temp_label_hold))})
-    print("these are in top_labels ",top_labels) 
+    print("these are in top_labels ",top_labels)
     #a set of the most popular labels
-    if len(top_labels) == 0:
+    if top_labels is False:
         return -1
-    else:
-        return random.choice(top_labels) #this is the label
+    return random.choice(top_labels) #this is the label
 
 def iterate_time(inp_labels, inp_nodes, connection_list, propagating_function):
     """Iterator for time
-    
+   
     Arguments:
         inp_labels {matrix} -- matrix with all current/past labels, ([:-1] is most recent)
         inp_nodes {list} -- list of nodes that need to be calculated
         connection_list {nested lists} -- defined in node_connections
         propagating_function {function} -- propagating function used
-    
+   
     Returns:
         matrix -- new set of labels, should be one more deep
     """
 
-    # check to insure the labels given match the expected shape 
+    # check to insure the labels given match the expected shape
     assert inp_labels.shape[0] == NUMBER_OF_NODES+1
     assert callable(propagating_function)
     node_order = shuffle_list(inp_nodes)
