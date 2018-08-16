28d818ff0d7c27bb860b76e191122fb12f70726b
Kang <christopher.kang@pnnl.gov>
2018-08-16 14:43:13 -0700

    Autopep8 Cleaned

--- prop_algs/graph_engine.py
+++ prop_algs/graph_engine.py
@@ -10,15 +10,15 @@ pd.read_csv()
 # used to create the feature columns necessary for the estimator
 def make_feature_columns(features):
     """Creates feature columns for an estimator
-    
+
     Arguments:
         features {pd df} -- dataframe with the entire matrix connection list
-    
+
     Returns:
         set -- set of tf feature columns (numeric)
     """
 
-    return set(tf.feature_column.numeric_column(feature) 
+    return set(tf.feature_column.numeric_column(feature)
                for feature in features)
 
 
@@ -36,8 +36,8 @@ def my_input_fn(features, labels, batch_size=1, shuffle=True):
 
 # need to write the model function
 def my_model_fn(features, labels, mode, params):
-    
-    # the feature column input layer 
+
+    # the feature column input layer
     # applies feature columns to the data dict
     net = tf.feature_column.input_layer(features, params['feature_columns'])
     for units in params['hidden_units']:
@@ -47,7 +47,7 @@ def my_model_fn(features, labels, mode, params):
     # the tf.nn.softmax can't be used as an activation function, so
     # it is applied afterwords
     logits = tf.layers.dense(net, params['n_classes'], activation=None)
-    
+
     # Predict
     predicted_classes = tf.argmax(logits, 1)
     if mode == tf.estimator.ModeKeys.PREDICT:
@@ -64,24 +64,22 @@ def my_model_fn(features, labels, mode, params):
     # Compute evaluation metrics
     accuracy = tf.metrics.accuracy(
         labels=labels, predictions=predicted_classes, name="acc_op")
-    
+
     metrics = {'accuracy': accuracy}
     tf.summary.scalar('accuracy', accuracy[1])
 
     if mode == tf.estimator.ModeKeys.EVAL:
         return tf.estimator.EstimatorSpec(
             mode, loss=loss, eval_metric_ops=metrics)
-    
+
     a_trainer = tf.train.AdagradOptimizer(0.1)
     train_op = a_trainer.minimize(loss, global_step=tf.train.get_global_step())
     return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
 
 
-
 classifier = tf.estimator.Estimator(
-    model_fn=my_model_fn, 
-    params={'feature_columns':  #make_feature_columns(),
+    model_fn=my_model_fn,
+    params={'feature_columns':  # make_feature_columns(),
             'hidden_units':  # ur code here [10, 10],
             'n_classes': 3,
-    })
-
+            })
