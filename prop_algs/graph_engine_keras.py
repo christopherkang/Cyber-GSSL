781ce7298b549663ed57450b4e74e7d0956980be
CK <kang828@we23533.pnl.gov>
2018-08-20 16:30:47 -0700

    Added training and one-hot

--- prop_algs/graph_engine_keras.py
+++ prop_algs/graph_engine_keras.py
@@ -1,10 +1,11 @@
+import os
+
 import keras as k
 import pandas as pd
-import os
 
 os.chdir(os.path.dirname(__file__))
 NUM_OF_LABELS = 10
-
+EPOCHS = 100
 # EDGE_MATRIX format: columns are connections, rows are individual nodes
 # values are weights
 EDGE_MATRIX = pd.read_pickle("../data/pandas_weight_array.pickle")
@@ -40,9 +41,36 @@ def init(layer_list):
     return model
 
 
-def train(neural_net, input_features, input_labels):
+def one_hot(labels):
+    """returns an array of one hot labels
 
-    neural_net.fit(input_features, input_labels, epochs=1,
-                   batch_size=input_features.shape[0])
+    Arguments:
+        labels {np array} -- input labels (categorical)
+
+    Returns:
+        np matrix -- one hot labels
+    """
+
+    one_hot_labels = k.utils.to_categorical(labels, num_classes=NUM_OF_LABELS)
+    return one_hot_labels
+
+
+def train(neural_net, input_features, input_labels):
+    """iteratively train a neural network
+
+    Arguments:
+        neural_net {model from K} -- model of neural network
+        input_features {np array?} -- input features
+        input_labels {matrix} -- matrix of features
+    """
+
+    neural_net.fit(input_features, input_labels,
+                   epochs=1, batch_size=input_features.shape[0])
+
+
+LABEL_LIST = one_hot(LABEL_LIST)
 
 NN = init([500, 50])
+
+for counter in range(EPOCHS):
+    train(NN,  , )
