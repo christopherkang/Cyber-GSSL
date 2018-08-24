6ad303ace7a51e9226885fbfcc12f37031e3c789
Kang <christopher.kang@pnnl.gov>
2018-08-24 10:18:49 -0700

    Updates

--- prop_algs/gell.py
+++ prop_algs/gell.py
@@ -19,7 +19,7 @@ NODE_CONNECTIONS = readfile.access_file("../data/node_connections.txt")
 TOTAL_LLUU_LIST = readfile.access_file("../data/total_edge_type.txt")
 LIST_OF_LABELED_EDGES = TOTAL_LLUU_LIST[0]
 LIST_OF_MIXED_EDGES = TOTAL_LLUU_LIST[1]
-LIST_OF_UNLABELED_EDGES = TOTAL_LLUU_LIST[2]
+LIST_OF_ALONE_EDGES = TOTAL_LLUU_LIST[2]
 
 NUM_OF_LABELS = 10
 
@@ -59,7 +59,8 @@ def g_theta(index):
     return tf.convert_to_tensor(random.choice(max_value))
 
 
-def h_theta(index):
+def h_theta(index, total_matrix):
+    tf.where(tf.equal(total_matrix))
     pass
 
 
@@ -83,7 +84,7 @@ def c_x(index, labels):
               tf.reduce_sum(tf.reduce_mean(labels * tf.log(g_theta(index)))))
 
 
-def custom_loss(u, v, labels, ):
+def custom_loss(labels, predicted):
 
     temp_sum = tf.convert_to_tensor(0)
     # iterate through each type of edge
@@ -93,13 +94,14 @@ def custom_loss(u, v, labels, ):
         temp_sum += tf.reduce_sum(
             ALPHA_1 * EDGE_MATRIX[u_pair, v_pair] *
             tf.norm(h_theta(u_pair)-h_theta(v_pair)) +
-            c_x(u, labels[u]) + c_x(v, labels[v]))
+            c_x(u_pair, labels[u_pair]) + c_x(v_pair, labels[v_pair]))
 
     for u_mixed, v_mixed in LIST_OF_MIXED_EDGES:
         # temp_sum = tf.add(temp_sum, tf.reduce_sum(ALPHA_2*))
         temp_sum += tf.reduce_sum(
             ALPHA_2 * EDGE_MATRIX[u_mixed, v_mixed] *
-            tf.norm(h_theta(u_mixed)-h_theta(v_mixed)))
+            tf.norm(h_theta(u_mixed) - h_theta(v_mixed)) +
+            c_x(u_mixed, labels[u_mixed]))
 
     for u_alone, v_alone in LIST_OF_ALONE_EDGES:
         # temp_sum = tf.add(temp_sum, tf.reduce_sum(ALPHA_3*))
@@ -121,7 +123,8 @@ def make_feature_col(features, range):
 def my_model_fn(dataset, hidden_nodes):
 
     net = tf.feature_column.input_layer(
-        dataset, make_feature_col(EDGE_MATRIX, [0, EDGE_MATRIX.shape[1]]))
+        dataset[:, :-2], make_feature_col(
+            EDGE_MATRIX, [0, EDGE_MATRIX.shape[1]]))
     for units in hidden_nodes:
         # then, pass the output through the hidden layers
         net = tf.layers.dense(net, units=units, activation=tf.nn.relu)
@@ -130,6 +133,10 @@ def my_model_fn(dataset, hidden_nodes):
     # it is applied afterwords
     logits = tf.layers.dense(
         net, params['n_classes'], activation=tf.nn.softmax)
+    
+    total_matrix = tf.concat(logits, dataset[:, -2])
+
+    
 
 
 
@@ -139,6 +146,6 @@ def my_model_fn(dataset, hidden_nodes):
 
 slices = tf.data.Dataset.from_tensor_slices(
     (EDGE_MATRIX.index.values, EDGE_MATRIX.values, LABEL_LIST.values))
-
+slices = slices.shuffle()
 slices = slices.batch(30).repeat(count=None)
 next_item = slices.make_one_shot_iterator().get_next()
